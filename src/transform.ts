3476ba3cf923cb054c34aab4a251db782bb23597
danielzhang183 <danielzhang182@gmail.com>
2023-06-06 13:37:16 +0800

    feat: enhance transform

--- src/transform.ts
+++ src/transform.ts
@@ -1,14 +1,26 @@
 import type { Node } from './parse'
 
 export interface TransformContext {
+  currentNode: Node | null
+  childIndex: number
+  parent: Node | null
+  replaceNode(node: Node, context: TransformContext): void
+  removeNode(context: TransformContext): void
   nodeTransforms: [
-    transformElement: (node: Node) => void,
-    transformText: (node: Node) => void,
+    transformElement: (node: Node) => OnExitFunction,
+    transformText: (node: Node) => OnExitFunction,
   ]
 }
 
+export type OnExitFunction = () => void
+
 export function transform(ast: Node) {
   const context: TransformContext = {
+    currentNode: null,
+    childIndex: 0,
+    parent: null,
+    replaceNode,
+    removeNode,
     nodeTransforms: [
       transformElement,
       transformText,
@@ -21,25 +33,56 @@ export function transform(ast: Node) {
 }
 
 function transformElement(node: Node) {
-  if (node.type === 'Element' && node.tag === 'p')
-    node.tag = 'h1'
+  return () => {
+    if (node.type === 'Element' && node.tag === 'p')
+      node.tag = 'h1'
+  }
 }
 
 function transformText(node: Node) {
-  if (node.type === 'Text')
-    node.content = node.content.repeat(2)
+  return () => {
+    if (node.type === 'Text')
+      node.content = node.content.repeat(2)
+  }
 }
 
 function traverseNode(ast: Node, context: TransformContext) {
-  const currentNode = ast
+  context.currentNode = ast
+  const exitFns: OnExitFunction[] = []
   const transforms = context.nodeTransforms
-  for (let i = 0; i < transforms.length; i++)
-    transforms[i](currentNode)
 
-  if ('children' in currentNode) {
-    const { children } = currentNode
-    for (let i = 0; i < children.length; i++)
+  for (let i = 0; i < transforms.length; i++) {
+    const onExit = transforms[i](context.currentNode)
+    if (onExit)
+      exitFns.push(onExit)
+    if (!context.currentNode)
+      return
+  }
+
+  if ('children' in context.currentNode) {
+    const { children } = context.currentNode
+    for (let i = 0; i < children.length; i++) {
+      context.parent = context.currentNode
+      context.childIndex = i
       traverseNode(children[i], context)
+    }
+  }
+
+  let i = exitFns.length
+  while (i--)
+    exitFns[i]()
+}
+
+function replaceNode(node: Node, context: TransformContext) {
+  context.currentNode = node
+  if (context.parent && 'children' in context.parent)
+    context.parent.children[context.childIndex] = node
+}
+
+function removeNode(context: TransformContext) {
+  if (context.parent && 'children' in context.parent) {
+    context.parent.children.splice(context.childIndex, 1)
+    context.currentNode = null
   }
 }
 
